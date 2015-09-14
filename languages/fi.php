9baa0e16ab6b0079007369911eb9639db91a9249
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-09-14 13:13:18 +0300

    Adds Finnish translations

---
+++ languages/fi.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Vazco text captcha english language file
+ */
+
+return array(
+	'vazco_text_captcha:entercaptcha' => 'Vastaa kysymykseen:',
+	'vazco_text_captcha:captchafail' => "Kysymyksen vastaus oli väärin.",
+	'vazco_text_captcha:tasks' => 'Määrittele captcha-kysymykset:',
+	'vazco_text_captcha:tasks:description' => 'Syötä kysymyksiä yksi per rivi. Erota kysymys vastauksesta | -merkillä (ilman välilyöntejä).',
+);
