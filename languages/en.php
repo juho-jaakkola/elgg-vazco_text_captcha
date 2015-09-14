488ed0ce63505a3fb9ea965f4bad8ec1b51f3263
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-09-14 13:10:15 +0300

    Converts languages file to the Elgg 1.9 format

--- languages/en.php
+++ languages/en.php
@@ -3,11 +3,9 @@
  * Vazco text captcha english language file
  */
 
-$english = array(
+return array(
 	'vazco_text_captcha:entercaptcha' => 'Please answer the question:',
 	'vazco_text_captcha:captchafail' => "Your answer to the question was incorrect.",
 	'vazco_text_captcha:tasks' => 'Define captcha questions:',
 	'vazco_text_captcha:tasks:description' => 'Enter questions one per line. Separate question from the answer with | character (do not add spaces around it).',
 );
-
-add_translation("en", $english);
